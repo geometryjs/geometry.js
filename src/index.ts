0de5ed284df641385e8597bea789eec0b7d174e7
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-04 16:40:20 +0200

    feat: Point class

--- src/index.ts
+++ src/index.ts
@@ -19,4 +19,22 @@ namespace GeometryJS {
         abstract get y(): number;
     }
 
+
+    export class Point extends PointBase {
+        public plane: Plane;
+        protected _x: number
+        protected _y: number
+        constructor(plane: Plane, x: number, y: number) {
+            super();
+            this.plane = plane;
+            this._x = x;
+            this._y = y;
+        }
+
+        get x(): number { return this._x; }
+        set x(value: number) { this._x = value; }
+
+        get y(): number { return this._y; }
+        set y(value: number) { this._y = value; }
+    }
 }
