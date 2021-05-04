f695083d97314c0be6f255f59e0a85cb021b85b7
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-04 16:32:48 +0200

    feat: Plane and Point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
 namespace GeometryJS {
-    
+    //! 2D
+    /**
+     * Class describing a 2D plane.
+     */
+    export class Plane {
+
+    }
+
+    //! Points
+    /**
+     * Default class describing a point
+     */
+    export class Point {
+        private _x: number;
+        private _y: number;
+        public plane: Plane;
+        constructor(plane: Plane, x: number, y: number) {
+            this.plane = plane;
+            this._x = x;
+            this._y = y;
+        }
+        
+        get x(): number { return this._x; }
+        set x(value: number) { this._x = value; }
+
+        get y(): number { return this._y; }
+        set y(value: number) { this._y = value; }
+    }
 }
