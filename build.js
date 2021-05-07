28cd29dabd1bc4daeec9b13e485cafe40304f498
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-07 08:57:36 +0200

    fix: updated build

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const childProcess = require('child_process');
 
 
-fs.unlinkSync("./src/index-module.ts");
+if (fs.existsSync("./src/index-module.ts")) fs.unlinkSync("./src/index-module.ts");
 fs.copyFileSync("./src/index.ts", "./src/index-module.ts");
 fs.appendFileSync("./src/index-module.ts", "\nexport default GeometryJS;");
 childProcess.execSync("tsc");
