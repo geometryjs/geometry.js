1665b5c3b8eb19c7a1503df6f91458f4d7e89198
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-07 16:37:16 +0200

    feat: tests for line and point intersects

--- test/tests.js
+++ test/tests.js
@@ -9,7 +9,19 @@ module.exports = [ // The tests
         assert(point.x, 20, "X stays same when point created.");
         assert(point.y, 10, "Y stays same when point created.");
     },
-    assert => { // The checks for point on line
+    assert => { // The checks for point line intersects
+        const plane = new GeometryJS.Plane();
+        const x = plane.createPoint(0, 0);
+        const y = plane.createPoint(100, 100);
+        const a = plane.createPoint(50, 50);
+        const b = plane.createPoint(50, 0);
+        const a2 = plane.createPoint(200, 200);
+        const b2 = plane.createPoint(0, 50);
         
+        const line = plane.createLine(x, y);
+        assert(line.intersects(x) && line.intersects(y), true, "Creator point lays on line");
+        assert(line.intersects(a), true, "Point between creator points lays on line");
+        assert(line.intersects(a2), true, "Point on line outside of segment lays on line.");
+        assert(line.intersects(b) || line.intersects(b2), false, "Point not on line does not intersect the line");
     }
 ]
