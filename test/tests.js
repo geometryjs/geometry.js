090d47780e5314147e595784f6053bb08637e1d6
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2021-05-07 23:33:36 +0200

    feat: parallel tests

--- test/tests.js
+++ test/tests.js
@@ -39,9 +39,28 @@ module.exports = [ // The tests
         assert(pLine.intersects(a), true, "Correct perpendicular line created");
         assert(pLine.intersects(b), true, "Correct perpendicular line created");
         assert(pLine.isParallel(line), false, "Checks, if perpendicular line is not parallel.");
+    },
+    assert => {
+        const plane = new GeometryJS.Plane();
 
+        const x = plane.createPoint(0, 0);
+        const y = plane.createPoint(100, 100);
+        const z = plane.createPoint(100, 0);
+        const line = plane.createLine(x, y);
+        const pLine = line.getParallel(z);
+
+        const ps = [-1000000000, -999800, - 10, 0, 50, 1e5, 1e8, 1e10].map(v => plane.createPoint(100 + v, v));
+        for (const p of ps) {
+            assert(pLine.intersects(p), true, "Point on the line: " + p.y);
+        }
+        const pns = [[10, 554], [599, 654], [561, -651], [236, -12]].map(v => plane.createPoint(...v));
+        for (const p of pns) {
+            assert(pLine.intersects(p), false, "Point non the line: " + p.xRounded);
+        }
+        assert(pLine.isParallel(line), true, "Parallel line check.");
     }
 ]
 module.exports[0].testName = "Value retainment";
 module.exports[1].testName = "Intersect check";
 module.exports[2].testName = "Perpendicular line check";
+module.exports[2].testName = "Parallel line check";
