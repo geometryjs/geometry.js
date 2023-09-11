8bf449e1e14d9a6e893533be6321a7e46b6c83d1
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2023-09-11 21:35:34 +0200

    interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,19 @@
+
+export const Cache = ["Cache"] as const;
+export const IterableCache = ["IterableCache", ...Cache] as const;
+export const Evaluatable = ["Evaluatable"] as const;
+export const DependencyNode = ["DependencyNode"] as const;
+export const GeometryObject = ["GeometryObject"] as const;
+export const Plane = ["Plane", ...GeometryObject, ...DependencyNode, ...IterableCache] as const;
+export const Procedure = ["Procedure"] as const;
+export const Transformer = ["Transformer"] as const;
+
+export type Interface =
+    | (typeof Cache)[number]
+    | (typeof IterableCache)[number]
+    | (typeof Evaluatable)[number]
+    | (typeof DependencyNode)[number]
+    | (typeof GeometryObject)[number]
+    | (typeof Plane)[number]
+    | (typeof Procedure)[number]
+    | (typeof Transformer)[number];
