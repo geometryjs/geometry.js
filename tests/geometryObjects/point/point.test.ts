0b22c3ea3d0435aea1143355a8de5de94d55e686
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2023-09-11 22:50:43 +0200

    point from two values test

---
+++ tests/geometryObjects/point/point.test.ts
@@ -0,0 +1,27 @@
+import { ReadonlyValue } from "../../../src/geometryObjects/value/readonlyValue";
+import { SettableValue } from "../../../src/geometryObjects/value/settableValue";
+
+import { PointFromTwoValues } from "../../../src/geometryObjects/point/pointFromTwoValues";
+describe("PointFromTwoValues", () => {
+    test("retains values", () => {
+        const x = new ReadonlyValue({ value: 1 });
+        const y = new ReadonlyValue({ value: 2 });
+
+        const point = new PointFromTwoValues({ x, y });
+
+        expect(point.x).toBe(1);
+        expect(point.y).toBe(2);
+    });
+    test("updates on set", () => {
+        const x = new SettableValue({ value: 1 });
+        const y = new SettableValue({ value: 2 });
+
+        const point = new PointFromTwoValues({ x, y });
+
+        x.value = 3;
+        y.value = 4;
+
+        expect(point.x).toBe(3);
+        expect(point.y).toBe(4);
+    });
+});
