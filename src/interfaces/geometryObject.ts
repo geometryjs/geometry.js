b3e97857c4d46d8656343eed9d2a8e107c97f0bf
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-09-12 09:59:43 +0200

    can cause update with tests

--- src/interfaces/geometryObject.ts
+++ src/interfaces/geometryObject.ts
@@ -20,4 +20,9 @@ export type GeometryObjectInfo = {
      * Interfaces implemented by this object.
      */
     implementedInterfaces: Iterable<Interface>;
+
+    /**
+     * Whether this object can trigger an update of the dependency tree. An object can usually trigger an update if it has settable values or non-pure methods.
+     */
+    canCauseUpdate: boolean;
 }
