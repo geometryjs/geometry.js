5248bdd1580365efb0016bb796e52ebbcc67fe4b
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-09-12 11:11:32 +0200

    test fixed with a plane

--- tests/geometryObjects/value/value.test.ts
+++ tests/geometryObjects/value/value.test.ts
@@ -1,7 +1,10 @@
+import { Plane } from "../../../src/geometryObjects/plane/plane";
+
 import { ReadonlyValue } from "../../../src/geometryObjects/value/readonlyValue";
 describe("ReadonlyValue", () => {
+    const plane = new Plane();
     test("retains value", () => {
-        const value = new ReadonlyValue({ value: 1 });
+        const value = new ReadonlyValue({ value: 1, plane });
         
         expect(value.value).toBe(1);
     });
@@ -9,14 +12,15 @@ describe("ReadonlyValue", () => {
 
 import { SettableValue } from "../../../src/geometryObjects/value/settableValue";
 describe("SettableValue", () => {
+    const plane = new Plane();
     test("retains value", () => {
-        const value = new SettableValue({ value: 1 });
+        const value = new SettableValue({ value: 1, plane });
         
         expect(value.value).toBe(1);
     });
 
     test("sets value", () => {
-        const value = new SettableValue({ value: 1 });
+        const value = new SettableValue({ value: 1, plane });
         value.value = 2;
         
         expect(value.value).toBe(2);
@@ -25,7 +29,7 @@ describe("SettableValue", () => {
     test("updates on set", () => {
         const mockUpdate = jest.fn();
 
-        const value = new SettableValue({ value: 1 });
+        const value = new SettableValue({ value: 1, plane });
         value.update = mockUpdate;
 
         value.value = 2;
