c5dcd9f95cf861e8ec2a89b8940d5409016dcba1
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2023-09-14 18:57:33 +0200

    docs for the module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 /**
+ * @module
+ * The main module of the library.
+ * The public API is exported from here as part of other namespaces or direct to use functions.
+ */
+
+/**
  * @namespace
- * Includes interfaces used by the library.  
+ * Includes interfaces used by the library.
  * Also includes constant, that are used to represent interfaces at runtime.
  */
 export * as Interfaces from "./interfaces";
