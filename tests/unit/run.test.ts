75b4f8b7f1dd3e35d1feef9771be56062c9ac462
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2023-09-14 20:10:30 +0200

    added few tests

--- tests/unit/run.test.ts
+++ tests/unit/run.test.ts
@@ -3,5 +3,10 @@ import * as GeometryJS from "../../src";
 describe("Library", () => {
     test("exports", () => {
         expect(GeometryJS).toBeDefined();
+        expect(GeometryJS.createPlane).toBeDefined();
+        expect(GeometryJS.Helpers).toBeDefined();
+        expect(GeometryJS.Interfaces).toBeDefined();
+        expect(GeometryJS.Objects).toBeDefined();
+        expect(GeometryJS.Procedures).toBeDefined();
     });
 });
