a1a65835a5f8ce4c97d7ffb770c43e8640282e93
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2023-09-17 09:18:35 +0200

    added groups to factories

--- src/factories.ts
+++ src/factories.ts
@@ -2,9 +2,11 @@ import { Plane } from "./geometryObjects";
 import { Plane as IPlane } from "./interfaces";
 
 /**
- * A factory function that creates a plane. All other objects are created from the plane.  
+ * A factory function that creates a plane. All other objects are created from the plane.
  * The plane is the only object that is not created from another object.
  * @returns An object, that implements the {@link IPlane | Plane} interface, currently {@link Plane | Plane}.
+ *
+ * @group Object creation
  */
 export function createPlane(): IPlane {
     return new Plane();
