654c76047ef244d1bc2f86bf5e3357d8e1417380
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-10-09 10:47:48 +0200

    unbound interval

---
+++ src/geometryObjects/interval/unboud.ts
@@ -0,0 +1,38 @@
+import type { Evaluatable, Interval, Plane } from "../../interfaces";
+
+export class UnboundInterval implements Interval, Evaluatable<number, boolean> {
+    public readonly start: number;
+    public readonly end: number;
+    public readonly startClosed: boolean;
+    public readonly endClosed: boolean;
+    constructor(parameters: {
+        start: {
+            value: number,
+            closed: boolean,
+        },
+        end: {
+            value: number,
+            closed: boolean,
+        }
+    }) {
+        this.start = parameters.start.value;
+        this.end = parameters.end.value;
+        this.startClosed = parameters.start.closed;
+        this.endClosed = parameters.end.closed;
+    }
+
+    public get length(): number {
+        return this.end - this.start;
+    }
+
+    isInside(value: number): boolean {
+        if (this.startClosed && value < this.start) return false;
+        if (!this.startClosed && value <= this.start) return false;
+        if (this.endClosed && value > this.end) return false;
+        if (!this.endClosed && value >= this.end) return false;
+        return true;
+    }
+    evaluate(input: number): boolean {
+        return this.isInside(input);
+    }
+}
