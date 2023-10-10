bc9df0bf79fdb813121e9ec5de28fbf05bf04c63
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-10-10 11:56:47 +0200

    constants

---
+++ src/constants/intervals.ts
@@ -0,0 +1,82 @@
+import { UnboundInterval } from "../geometryObjects/interval/unboud";
+import { NEGATIVE_INFINITY, POSITIVE_INFINITY } from "./numbers";
+
+/**
+ * The interval of all real numbers.
+ * 
+ * @group Interval constants
+ */
+export const REALS = new UnboundInterval({
+    end: {
+        closed: false,
+        value: POSITIVE_INFINITY
+    },
+    start: {
+        closed: false,
+        value: NEGATIVE_INFINITY
+    }
+});
+
+/**
+ * The interval of all positive real numbers.
+ * 
+ * @group Interval constants
+ */
+export const POSITIVE_REALS = new UnboundInterval({
+    end: {
+        closed: false,
+        value: POSITIVE_INFINITY
+    },
+    start: {
+        closed: false,
+        value: 0
+    }
+});
+
+/**
+ * The interval of all negative real numbers.
+ * 
+ * @group Interval constants
+ */
+export const NEGATIVE_REALS = new UnboundInterval({
+    end: {
+        closed: false,
+        value: 0
+    },
+    start: {
+        closed: false,
+        value: NEGATIVE_INFINITY
+    }
+});
+
+/**
+ * The interval of all non-negative real numbers.
+ * 
+ * @group Interval constants
+ */
+export const NOT_NEGATIVE_REALS = new UnboundInterval({
+    end: {
+        closed: false,
+        value: POSITIVE_INFINITY
+    },
+    start: {
+        closed: true,
+        value: 0
+    }
+});
+
+/**
+ * The interval of all non-positive real numbers.
+ * 
+ * @group Interval constants
+ */
+export const NOT_POSITIVE_REALS = new UnboundInterval({
+    end: {
+        closed: true,
+        value: 0
+    },
+    start: {
+        closed: false,
+        value: NEGATIVE_INFINITY
+    }
+});
