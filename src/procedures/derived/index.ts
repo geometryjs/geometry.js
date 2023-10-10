eaa3bc2194d0b6341c689b69bcf96ed186cfd501
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-10-10 11:35:53 +0200

    reexport

--- src/procedures/derived/index.ts
+++ src/procedures/derived/index.ts
@@ -1,6 +1,14 @@
+import { LineCCoefficient } from "./lineEquation";
 import { PerpendicularVector } from "./perpendicularVector";
 
 /**
  * Creates a vector perpendicular to the given vector.
+ * @see {@link PerpendicularVector}
  */
 export const PERPENDICULAR_VECTOR = new PerpendicularVector();
+
+/**
+ * Calculates the c coefficient of the line equation in the form of ax + by + c = 0, given the line's normal vector and a point on the line.
+ * @see {@link LineCCoefficient}
+ */
+export const LINE_C_COEFFICIENT = new LineCCoefficient();
