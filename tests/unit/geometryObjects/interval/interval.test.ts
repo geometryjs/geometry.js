e0dd5468c2d0eb8adb30c92adc45dc68bf851459
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-10-20 18:10:50 +0200

    interval test

--- tests/unit/geometryObjects/interval/interval.test.ts
+++ tests/unit/geometryObjects/interval/interval.test.ts
@@ -1,6 +1,7 @@
+import { UnboundInterval } from "../../../../src/geometryObjects/interval"
 describe("UnboundInterval", () => {
     test("can be created", () => {
-        const interval = new UnboundIn({
+        const interval = new UnboundInterval({
             start: {
                 value: 1,
                 closed: true,
@@ -11,5 +12,67 @@ describe("UnboundInterval", () => {
             }
         });
         expect(interval).toBeDefined();
+    });
+
+    test.each([
+        {
+            number: 1,
+            expected: true,
+        },
+        {
+            number: 2,
+            expected: false,
+        },
+        {
+            number: 3,
+            expected: false,
+        },
+        {
+            number: 0,
+            expected: false,
+        }
+    ])(`isInside`, ({ number, expected }) => {
+        const interval = new UnboundInterval({
+            start: {
+                value: 1,
+                closed: true,
+            },
+            end: {
+                value: 2,
+                closed: false,
+            }
+        });
+        expect(interval.isInside(number)).toBe(expected);
+    });
+
+    test("length", () => {
+        const interval = new UnboundInterval({
+            start: {
+                value: 1,
+                closed: true,
+            },
+            end: {
+                value: 2,
+                closed: false,
+            }
+        });
+        expect(interval.length).toBe(1);
+    });
+
+    test("evaluate", () => {
+        const interval = new UnboundInterval({
+            start: {
+                value: 1,
+                closed: false,
+            },
+            end: {
+                value: 2,
+                closed: true,
+            }
+        });
+        expect(interval.evaluate(1)).toBe(false);
+        expect(interval.evaluate(2)).toBe(true);
+        expect(interval.evaluate(3)).toBe(false);
+        expect(interval.evaluate(0)).toBe(false);
     });
 });
