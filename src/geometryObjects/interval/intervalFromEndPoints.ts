b8825c7802a898d96cc0a9deedbca334d8b3c17e
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-11 16:17:42 +0100

    added specific open and closed intervals specified by end points

--- src/geometryObjects/interval/intervalFromEndPoints.ts
+++ src/geometryObjects/interval/intervalFromEndPoints.ts
@@ -1,7 +1,11 @@
 import type { Plane } from "../../interfaces";
+
 import { AbstractInterval } from "./abstractInterval";
 
-class IntervalFromEndPoints extends AbstractInterval {
+/**
+ * An interval defined by its end points
+ */
+export class IntervalFromEndPoints extends AbstractInterval {
     private startValue: number;
     private endValue: number;
     private startIncludedValue: boolean;
@@ -56,6 +60,90 @@ class IntervalFromEndPoints extends AbstractInterval {
     }
     public set endIncluded(value: boolean) {
         this.endIncludedValue = value;
+        this.update();
+    }
+}
+
+/**
+ * An open interval defined by its end points
+ */
+export class OpenIntervalFromEndPoints extends AbstractInterval {
+    private startValue: number;
+    private endValue: number;
+
+    constructor(parameters: { start: number, end: number, plane: Plane}) {
+        super({ dependencies: [], ...parameters });
+        this.startValue = parameters.start;
+        this.endValue = parameters.end;
+    }
+    getStart() {
+        return this.startValue;
+    }
+    getEnd() {
+        return this.endValue;
+    }
+    getStartIncluded() {
+        return false;
+    }
+    getEndIncluded() {
+        return false;
+    }
+
+    public get start() { // Do not remove this, when overriding a setter, the getter must be overridden too
+        return super.start;
+    }
+    public set start(value: number) {
+        this.startValue = value;
+        this.update();
+    }
+
+    public get end() { // Do not remove this, when overriding a setter, the getter must be overridden too
+        return super.end;
+    }
+    public set end(value: number) {
+        this.endValue = value;
+        this.update();
+    }
+}
+
+/**
+ * A closed interval defined by its end points
+ */
+export class ClosedIntervalFromEndPoints extends AbstractInterval {
+    private startValue: number;
+    private endValue: number;
+
+    constructor(parameters: { start: number, end: number, plane: Plane}) {
+        super({ dependencies: [], ...parameters });
+        this.startValue = parameters.start;
+        this.endValue = parameters.end;
+    }
+    getStart() {
+        return this.startValue;
+    }
+    getEnd() {
+        return this.endValue;
+    }
+    getStartIncluded() {
+        return true;
+    }
+    getEndIncluded() {
+        return true;
+    }
+
+    public get start() { // Do not remove this, when overriding a setter, the getter must be overridden too
+        return super.start;
+    }
+    public set start(value: number) {
+        this.startValue = value;
+        this.update();
+    }
+
+    public get end() { // Do not remove this, when overriding a setter, the getter must be overridden too
+        return super.end;
+    }
+    public set end(value: number) {
+        this.endValue = value;
         this.update();
     }
 }
