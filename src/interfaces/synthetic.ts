5042a014ab677b147d3753c1d6135c11976a9434
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-19 17:33:58 +0100

    synthetic object interfaces

--- src/interfaces/synthetic.ts
+++ src/interfaces/synthetic.ts
@@ -10,7 +10,7 @@ import type { SingleParametricCurve } from "./parametricCurve";
 import type { Point, SettablePoint } from "./point";
 import type { SettableValue, Value } from "./value";
 import type { SettableVector, Vector } from "./vector";
-import type { Line } from "./line";
+import type { Line, LineWithSettableArbitraryPoints, LineWithSettableDirectionalVector, LineWithSettableEquation, LineWithSettableNormalVector } from "./line";
 import { Evaluatable } from "./evaluatable";
 
 /**
@@ -18,84 +18,123 @@ import { Evaluatable } from "./evaluatable";
  *
  * @group Synthetic
  */
-export interface ValueObject extends Value, DependencyNode, GeometryObject {}
+export interface ValueObject extends Value, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link SettableValue}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface SettableValueObject extends SettableValue, DependencyNode, GeometryObject {}
+export interface SettableValueObject extends SettableValue, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link Point}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface PointObject extends Point, DependencyNode, GeometryObject {}
+export interface PointObject extends Point, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link Vector}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface VectorObject extends Vector, DependencyNode, GeometryObject {}
+export interface VectorObject extends Vector, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link SettableVector}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface SettableVectorObject extends SettableVector, DependencyNode, GeometryObject {}
+export interface SettableVectorObject extends SettableVector, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link Interval}, {@link Evaluatable}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface IntervalObject extends Interval, DependencyNode, GeometryObject, Evaluatable<number, boolean> {}
+export interface IntervalObject extends Interval, DependencyNode, GeometryObject, Evaluatable<number, boolean> { }
 
 /**
  * Interface, that combines the {@link SingleParametricCurve}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface SingleParametricCurveObject extends SingleParametricCurve, DependencyNode, GeometryObject {}
+export interface SingleParametricCurveObject extends SingleParametricCurve, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link Line}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface LineObject extends Line, DependencyNode, GeometryObject {}
+export interface LineObject extends Line, DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link DependencyNode} and {@link GeometryObject} interfaces.
  *
  * @group Synthetic
  */
-export interface DependencyNodeObject extends DependencyNode, GeometryObject {}
+export interface DependencyNodeObject extends DependencyNode, GeometryObject { }
 
 /**
  * Interface, that combines the {@link IntervalWithSettableEndpoints}, {@link Evaluatable}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  * 
  * @group Synthetic
  */
-export interface IntervalWithSettableEndpointsObject extends IntervalWithSettableEndpoints, DependencyNode, GeometryObject, Evaluatable<number, boolean> {}
+export interface IntervalWithSettableEndpointsObject extends IntervalWithSettableEndpoints, DependencyNode, GeometryObject, Evaluatable<number, boolean> { }
 
 /**
  * Interface, that combines the {@link IntervalWithSettableEndpointsInclusion}, {@link Evaluatable}, {@link DependencyNode} and {@link GeometryObject} interfaces.
  * 
  * @group Synthetic
  */
-export interface IntervalWithSettableEndpointsInclusionObject extends IntervalWithSettableEndpointsInclusion, DependencyNode, GeometryObject, Evaluatable<number, boolean> {}
+export interface IntervalWithSettableEndpointsInclusionObject extends IntervalWithSettableEndpointsInclusion, DependencyNode, GeometryObject, Evaluatable<number, boolean> { }
 
 /**
  * Interface, that combines the {@link IntervalWithSettableEndpointsValue}, {@link Evaluatable}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ * @group Synthetic
  */
-export interface IntervalWithSettableEndpointsValueObject extends IntervalWithSettableEndpointsObject, DependencyNode, GeometryObject, Evaluatable<number, boolean> {}
+export interface IntervalWithSettableEndpointsValueObject extends IntervalWithSettableEndpointsObject, DependencyNode, GeometryObject, Evaluatable<number, boolean> { }
 
 /**
  * Interface, that combines the {@link SettablePoint}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ * @group Synthetic
  */
-export interface SettablePointObject extends SettablePoint, DependencyNode, GeometryObject {}
+export interface SettablePointObject extends SettablePoint, DependencyNode, GeometryObject { }
+
+/**
+ * Interface, that combines the {@link LineWithSettableEquation}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ *  @group Synthetic
+ */
+export interface LineWithSettableEquationObject extends LineWithSettableEquation, DependencyNode, GeometryObject { }
+
+/**
+ * Interface, that combines the {@link LineWithSettableArbitraryPoints}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ *  @group Synthetic
+ */
+export interface LineWithSettableArbitraryPointsObject extends LineWithSettableArbitraryPoints, DependencyNode, GeometryObject {}
+
+/**
+ * Interface, that combines the {@link LineWithSettableDirectionalVector}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ * @group Synthetic
+ */
+export interface LineWithSettableDirectionalVectorObject extends LineWithSettableDirectionalVector, DependencyNode, GeometryObject {}
+
+/**
+ * Interface, that combines the {@link LineWithSettableNormalVector}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ * @group Synthetic
+ */
+export interface LineWithSettableNormalVectorObject extends LineWithSettableNormalVector, DependencyNode, GeometryObject {}
+
+/**
+ * Interface, that combines the {@link LineWithSettableXAxisAngle}, {@link DependencyNode} and {@link GeometryObject} interfaces.
+ * 
+ * @group Synthetic
+ */
+export interface LineWithSettableXAxisAngleObject extends LineWithSettableDirectionalVectorObject, DependencyNode, GeometryObject {}
