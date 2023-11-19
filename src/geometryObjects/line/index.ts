388a73be2f3bb30eeee95562352140aaa59cc95e
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-19 17:56:18 +0100

    line reexports

--- src/geometryObjects/line/index.ts
+++ src/geometryObjects/line/index.ts
@@ -1,1 +1,9 @@
 export * from "./abstractLine";
+
+export * from "./lineFromTwoPoints";
+export * from "./lineFromEquation";
+
+
+export * from "./parallelLine";
+export * from "./perpendicularLine";
+
