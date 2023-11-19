e6a36b9af9fcb16731d4c47aab40cd957dbd7e5a
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-19 17:59:00 +0100

    line from point and directional vector

--- src/geometryObjects/line/index.ts
+++ src/geometryObjects/line/index.ts
@@ -3,6 +3,7 @@ export * from "./abstractLine";
 export * from "./lineFromTwoPoints";
 export * from "./lineFromEquation";
 export * from "./lineFromEquationAsValues";
+export * from "./lineFromPointAndDirectionalVector";
 
 export * from "./parallelLine";
 export * from "./perpendicularLine";
---
+++ src/geometryObjects/line/lineFromPointAndDirectionalVector.ts
@@ -0,0 +1,26 @@
+import { Point, PointObject, Vector, VectorObject } from "../../interfaces";
+import { Plane } from "../plane";
+import { AbstractLineFromPointAndDirectionalVector } from "./abstractLine";
+
+/**
+ * A line defined by a point and a directional vector.
+ */
+export class LineFromPointAndDirectionalVector extends AbstractLineFromPointAndDirectionalVector {
+    private pointValue: Point;
+    private directionalVectorValue: Vector;
+
+    constructor(parameters: { point: PointObject, directionalVector: VectorObject, plane: Plane }) {
+        super({ dependencies: [parameters.point, parameters.directionalVector], ...parameters });
+
+        this.pointValue = parameters.point;
+        this.directionalVectorValue = parameters.directionalVector;
+    }
+
+    protected getPoint(): Point {
+        return this.pointValue;
+    }
+
+    protected getDirectionalVector(): Vector {
+        return this.directionalVectorValue;
+    }
+}
