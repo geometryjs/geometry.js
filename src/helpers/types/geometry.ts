a4c76dedf000dd287d9a8d0c987041be6d943f1c
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-10 21:47:26 +0100

    updated to include derived objects

--- src/helpers/types/geometry.ts
+++ src/helpers/types/geometry.ts
@@ -1,4 +1,4 @@
-import type { Interval, Line, Plane, Point, Value, Vector } from "../../interfaces";
+import type { DependencyNodeObject, EnumObject, Interval, Line, Plane, Point, Union, Value, Vector } from "../../interfaces";
 
 /**
  * A virtual object is a something that can be a geometry object, but doesn't have a position in space.
@@ -8,4 +8,4 @@ export type VirtualObject = Value | Interval | Vector;
 /**
  * A non-virtual object is a something that can be a geometry object, and has a position in space.
  */
-export type NonVirtualObject = Point | Line | Plane;
+export type NonVirtualObject = Point | Line | Plane | EnumObject<Record<string, NonVirtualObject & DependencyNodeObject>> | Union<NonVirtualObject, NonVirtualObject[]>;
