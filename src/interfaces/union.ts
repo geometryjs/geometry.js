665939d6645cb5b1dfa4bc803df4b587bccc4a14
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-10 19:05:37 +0100

    documentation fixes

--- src/interfaces/union.ts
+++ src/interfaces/union.ts
@@ -1,6 +1,7 @@
 
 /**
- * A union is a collection of objects that are combined into a single object.
+ * A union is a collection of objects that are combined into a single object.  
+ * When returned by a function or method, it is to be handled as a single object.
  */
 export interface Union<Object, Objects extends Object[]> { 
     /**
