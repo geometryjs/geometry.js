8fb34ec730590991d6d1e8bcd73987153e32158f
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-10 21:52:01 +0100

    added string identifiers

--- src/helpers/types/geometry.ts
+++ src/helpers/types/geometry.ts
@@ -6,6 +6,16 @@ import type { DependencyNodeObject, EnumObject, Interval, Line, Plane, Point, Un
 export type VirtualObject = Value | Interval | Vector;
 
 /**
+ * A virtual object identifier is a string, that identifies a virtual object.
+ */
+export type VirtualObjectIdentifier = "value" | "interval" | "vector";
+
+/**
  * A non-virtual object is a something that can be a geometry object, and has a position in space.
  */
 export type NonVirtualObject = Point | Line | Plane | EnumObject<Record<string, NonVirtualObject & DependencyNodeObject>> | Union<NonVirtualObject, NonVirtualObject[]>;
+
+/**
+ * A non-virtual object identifier is a string, that identifies a non-virtual object.
+ */
+export type NonVirtualObjectIdentifier = "point" | "line" | "plane" | "enum" | "union";
