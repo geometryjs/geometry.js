ba10fb107aaea71e8caecd6216b9e3632474337b
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-16 20:05:24 +0100

    Add LineFromLineDistance procedure

--- src/procedures/foundational/distance.ts
+++ src/procedures/foundational/distance.ts
@@ -1,14 +1,15 @@
+import { LINES_PARALLEL } from "../derived/lineEquation";
 import { Procedure } from "../procedure";
 
 /**
  * Calculates the distance between a point and a line.
  * @see {@link https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line}
  */
-export class PointFromLineDistance extends Procedure<{point: {x: number, y: number}, line: {a: number, b: number, c:number}}, {distance: number}> {
+export class PointFromLineDistance extends Procedure<{ point: { x: number, y: number }, line: { a: number, b: number, c: number } }, { distance: number }> {
     constructor() {
         super({
             name: "Point From Line Distance",
-            procedure: ({point, line}) => {
+            procedure: ({ point, line }) => {
                 // TODO: Handle case where line is made of zero vectors
                 return {
                     distance: Math.abs(line.a * point.x + line.b * point.y + line.c) / Math.sqrt(line.a ** 2 + line.b ** 2)
@@ -22,3 +23,25 @@ export class PointFromLineDistance extends Procedure<{point: {x: number, y: numb
  * Calculates the distance between a point and a line.
  */
 export const POINT_FROM_LINE_DISTANCE = new PointFromLineDistance();
+
+/**
+ * Calculates the distance between two lines.
+ * @see {@link https://en.wikipedia.org/wiki/Distance_between_two_parallel_lines}
+ */
+export class LineFromLineDistance extends Procedure<{ line1: { a: number, b: number, c: number }, line2: { a: number, b: number, c: number } }, { distance: number }> {
+    constructor() {
+        super({
+            name: "Line From Line Distance",
+            procedure: ({ line1, line2 }) => { 
+                if (!LINES_PARALLEL.perform({ lines: [line1, line2] }).parallel) return { distance: 0 };
+                return { distance: Math.abs(line1.c - line2.c) / Math.sqrt(line1.a ** 2 + line1.b ** 2) };
+            }
+        });
+    }
+}
+
+/**
+ * Calculates the distance between two lines.
+ * @see {@link LineFromLineDistance}
+ */
+export const LINE_FROM_LINE_DISTANCE = new LineFromLineDistance();
--- tests/unit/procedures/distances.test.ts
+++ tests/unit/procedures/distances.test.ts
@@ -1,4 +1,4 @@
-import { POINT_FROM_LINE_DISTANCE } from "../../../src/procedures/foundational";
+import { LINE_FROM_LINE_DISTANCE, POINT_FROM_LINE_DISTANCE } from "../../../src/procedures/foundational";
 
 describe("Line Point Distance", () => {
     const linePointPairs = [
@@ -74,11 +74,192 @@ describe("Line Point Distance", () => {
             },
             distance: 0,
         },
-        
-        
-    ] satisfies {line: {a: number, b: number, c: number}, point: {x: number, y: number}, distance: number}[];
 
-    test.each(linePointPairs)("Distance from line %p to point %p should be %p", ({line, point, distance}) => {
-        expect(POINT_FROM_LINE_DISTANCE.perform({line, point}).distance).toBeCloseTo(distance);
+
+    ] satisfies { line: { a: number, b: number, c: number }, point: { x: number, y: number }, distance: number }[];
+
+    test.each(linePointPairs)("Distance from line %p to point %p should be %p", ({ line, point, distance }) => {
+        expect(POINT_FROM_LINE_DISTANCE.perform({ line, point }).distance).toBeCloseTo(distance);
     });
+});
+
+describe("Line Line Distance", () => {
+    const lineLinePairs = [
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: 0,
+                b: 1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: 1,
+                b: 0,
+                c: 1,
+            },
+            distance: 1,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: 1,
+                b: -1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: -1,
+                b: -1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: 0,
+                b: 1,
+                c: 1,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: -1,
+                b: 1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: 1,
+                b: 1,
+                c: 1,
+            },
+            distance: 1 / Math.sqrt(2),
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: -1,
+                b: 1,
+                c: 0,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: 1,
+                b: 1,
+                c: 1,
+            },
+            distance: 1 / Math.sqrt(2),
+        },
+        {
+            line1: {
+                a: 1,
+                b: 1,
+                c: 0,
+            },
+            line2: {
+                a: 0,
+                b: 1,
+                c: 1,
+            },
+            distance: 0,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: -1,
+                b: 0,
+                c: 1,
+            },
+            distance: 1,
+        },
+        {
+            line1: {
+                a: 1,
+                b: 0,
+                c: 0,
+            },
+            line2: {
+                a: 0,
+                b: -1,
+                c: 1,
+            },
+            distance: 0,
+        },
+    ];
+
+
+    test.each(lineLinePairs)(`should calculate the distance between lines}`, (pair) => {
+        const distance = LINE_FROM_LINE_DISTANCE.perform(pair).distance;
+        expect(distance).toBeCloseTo(pair.distance);
+    });
+
 });
