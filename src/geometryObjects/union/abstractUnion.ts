29607fdae51d52b504a3e9c511c97656feba8187
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-17 17:06:54 +0100

    Update AbstractUnion class to include NonVirtualGeometryObject in type declaration

--- src/geometryObjects/union/abstractUnion.ts
+++ src/geometryObjects/union/abstractUnion.ts
@@ -1,12 +1,12 @@
 import type { Some } from "../../helpers/types";
-import { Runtime, type DependencyNodeObject, type IterableCache, type Plane, type Union } from "../../interfaces";
+import { Runtime, type DependencyNodeObject, type IterableCache, type Plane, type Union, NonVirtualGeometryObject } from "../../interfaces";
 
 import { GeometryObject } from "../geometryObject";
 
 /**
  * Abstract base class for unions.
  */
-export abstract class AbstractUnion<Objects extends GeometryObject[], CacheRecords extends Record<string, Some | null> = Record<string, Some | null>> extends GeometryObject<CacheRecords> implements Union<Objects> {
+export abstract class AbstractUnion<Objects extends NonVirtualGeometryObject[], CacheRecords extends Record<string, Some | null> = Record<string, Some | null>> extends GeometryObject<CacheRecords> implements Union<Objects> {
     protected abstract getObjects(): Objects;
     
     constructor(parameters: { dependencies: DependencyNodeObject[], plane: Plane, cache: IterableCache<CacheRecords, true>, implementedInterfaces: Iterable<Runtime.Interface> }) {
