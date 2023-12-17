0ae015542995d5f9c3bdc50515b1934b69864d01
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-17 16:45:53 +0100

    Add static methods to create virtual and non-virtual null objects

--- src/geometryObjects/nullObject/unboundNullObject.ts
+++ src/geometryObjects/nullObject/unboundNullObject.ts
@@ -9,4 +9,20 @@ export class UnboundNullObject implements NullObject {
     }
     public readonly objectType = "null";
     public readonly virtual: boolean;
+
+    /**
+     * Creates a virtual null object.
+     * @returns A virtual null object.
+     */
+    static createVirtual(): UnboundNullObject {
+        return new UnboundNullObject({ virtual: true });
+    }
+
+    /**
+     * Creates a non-virtual null object.
+     * @returns A non-virtual null object.
+     */
+    static createNonVirtual(): UnboundNullObject {
+        return new UnboundNullObject({ virtual: false });
+    }
 }
