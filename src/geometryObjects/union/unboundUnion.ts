412594af293feb51f8b010850ee149dfe106c4fa
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-17 17:07:49 +0100

    Update UnboundUnion class to use NonVirtualObject interface

--- src/geometryObjects/union/unboundUnion.ts
+++ src/geometryObjects/union/unboundUnion.ts
@@ -1,9 +1,9 @@
-import type { ObjectWithType, Union } from "../../interfaces";
+import type { NonVirtualObject, ObjectWithType, Union } from "../../interfaces";
 
 /**
  * A union of objects, that are not bound to a plane.
  */
-export class UnboundUnion<Objects extends ObjectWithType[]> implements Union<Objects> {
+export class UnboundUnion<Objects extends NonVirtualObject[]> implements Union<Objects> {
 
     constructor(public readonly objects: Objects) { }
 
