7d9f021b43efd578857a1b7ff016d4d3fca4aade
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-18 16:36:35 +0100

    Refactor UnboundNullObject class to support generic virtual parameter

--- src/geometryObjects/nullObject/unboundNullObject.ts
+++ src/geometryObjects/nullObject/unboundNullObject.ts
@@ -3,18 +3,18 @@ import type { NullObject } from "../../interfaces";
 /**
  * A null object, that is not bound to a plane.
  */
-export class UnboundNullObject implements NullObject {
-    constructor(parameters: { virtual: boolean}) {
+export class UnboundNullObject<Virtual extends boolean = boolean> implements NullObject<Virtual> {
+    constructor(parameters: { virtual: Virtual }) {
         this.virtual = parameters.virtual;
     }
     public readonly objectType = "null";
-    public readonly virtual: boolean;
+    public readonly virtual: Virtual;
 
     /**
      * Creates a virtual null object.
      * @returns A virtual null object.
      */
-    static createVirtual(): UnboundNullObject {
+    static createVirtual(): UnboundNullObject<true> {
         return new UnboundNullObject({ virtual: true });
     }
 
@@ -22,7 +22,7 @@ export class UnboundNullObject implements NullObject {
      * Creates a non-virtual null object.
      * @returns A non-virtual null object.
      */
-    static createNonVirtual(): UnboundNullObject {
+    static createNonVirtual(): UnboundNullObject<false> {
         return new UnboundNullObject({ virtual: false });
     }
 }
