60152cc9625ecc89ecd5532c758a9f4c1da6201d
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-18 16:50:50 +0100

    Add LINES_PARALLEL export to lineEquation module

--- src/procedures/derived/index.ts
+++ src/procedures/derived/index.ts
@@ -1,4 +1,4 @@
-export { LINE_C_COEFFICIENT } from "./lineEquation";
+export { LINE_C_COEFFICIENT, LINES_PARALLEL } from "./lineEquation";
 export { PERPENDICULAR_VECTOR } from "./perpendicularVector";
 export { TWO_POINTS_ON_EQUATION_LINE } from "./pointsOnLine";
 
